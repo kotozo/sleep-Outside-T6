fc69e1f1de328cd0f89ab0dea0ceefb2982b9f4e
kotozo <toz20002@byui.edu>
2025-11-29 16:52:01 +0000

    update product.js@29112025

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,13 @@
 import { setLocalStorage, getParam } from "./utils.mjs"; //Import setLocalStorage and getParam function from utils.mjs into product.js.
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-console.log(dataSource.findProductById(productId));
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
