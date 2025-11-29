fde839137a9d6485512085feb6d7f8d18e58253d
kotozo <toz20002@byui.edu>
2025-11-29 14:49:16 +0000

    update productDetails()

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -43,3 +43,23 @@ function productDetailsTemplate(product) {
 
     document.getElementById('addToCart').dataset.id = product.id;
 }
+
+/*=======alternative for the productDetailsTemplate()==========
+ function productDetailsTemplate(product) {
+    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+    class="divider"
+    src="${product.Image}"
+    alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+    <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+ } 
+===============*/
