7bae312811775749b71d198156709008730c0d52
kotozo <toz20002@byui.edu>
2025-11-29 15:08:12 +0000

    update utils.mjs

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,8 +21,20 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-getParam(param){
+
+// get the product id from the query string
+export function getParam(param){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param)
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, List, Position = "afterbegin", clear = false){
+  const htmlStrings = List.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
