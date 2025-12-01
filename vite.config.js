9ea72e2bc37290d6a2261271b7ec26acb979a432
kotozo <toz20002@byui.edu>
2025-12-01 20:16:33 +0000

    update 1/12/2025

--- vite.config.js
+++ vite.config.js
@@ -29,38 +29,4 @@ export default defineConfig({
   },
 },
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-/*
-  build: {
-    outDir: "../dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_page/index.html"
-       
-        ),
-      },
-    },
-  },
-});
-*/
+})
